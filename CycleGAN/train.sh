2b5bcaeae3b7815d5c0f7886d5c35e5d4a22e342
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2025-05-08 18:53:12 +0200

    Revert "init methods"
    This reverts commit 7e85de73514d99c725f87c807eaffd8ad3c88bc6.

--- CycleGAN/train.sh
+++
@@ -1,7 +0,0 @@
-!git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
-%cd pytorch-CycleGAN-and-pix2pix
-!pip install dominate visdom
-
-!bash ./datasets/download_cyclegan_dataset.sh horse2zebra
-
-!python train.py --dataroot ./datasets/horse2zebra --name horse2zebra_cyclegan --model cycle_gan
